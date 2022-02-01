8e4a7f0582100078b543b4084ea4c4815b03bc40
unknown <jpearlz0123@gmail.com>
2022-02-02 03:00:43 +0800

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,382 @@
-import logo from './logo.svg';
+import react, { Component } from 'react';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-tsparticles';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ParticlesOptions = {
+  "autoPlay": true,
+  "background": {
+    "color": {
+    },
+    "image": "",
+    "position": "50% 50%",
+    "repeat": "no-repeat",
+    "size": "cover",
+    "opacity": 1
+  },
+  "detectRetina": true,
+  "duration": 0,
+  "fpsLimit": 60,
+  "interactivity": {
+    "detectsOn": "window",
+    "events": {
+      "onClick": {
+        "enable": false,
+        "mode": []
+      },
+      "onDiv": {
+        "selectors": [],
+        "enable": false,
+        "mode": [],
+        "type": "circle"
+      },
+      "onHover": {
+        "enable": false,
+        "mode": [],
+        "parallax": {
+          "enable": false,
+          "force": 2,
+          "smooth": 10
+        }
+      },
+      "resize": true
+    },
+    "modes": {
+      "attract": {
+        "distance": 200,
+        "duration": 0.4,
+        "easing": "ease-out-quad",
+        "factor": 1,
+        "maxSpeed": 50,
+        "speed": 1
+      },
+      "light": {
+        "area": {
+          "gradient": {
+            "start": {
+              "value": "#ffffff"
+            },
+            "stop": {
+              "value": "#000000"
+            }
+          },
+          "radius": 1000
+        },
+      },
+      "trail": {
+        "delay": 1,
+        "pauseOnStop": false,
+        "quantity": 1
+      }
+    }
+  },
+  "particles": {
+    "bounce": {
+      "horizontal": {
+        "random": {
+          "enable": false,
+          "minimumValue": 0.1
+        },
+        "value": 1
+      },
+    },
+    "collisions": {
+      "bounce": {
+        "horizontal": {
+          "random": {
+            "enable": false,
+            "minimumValue": 0.1
+          },
+          "value": 1
+        },
+        "vertical": {
+          "random": {
+            "enable": false,
+            "minimumValue": 0.1
+          },
+          "value": 1
+        }
+      },
+
+    },
+    "color": {
+      "value": "#fff",
+      "animation": {
+        "h": {
+          "count": 0,
+          "enable": false,
+          "offset": 0,
+          "speed": 1,
+          "sync": true
+        },
+      }
+    },
+    "links": {
+      "blink": false,
+      "color": {
+        "value": "#fff"
+      },
+      "consent": false,
+      "distance": 150,
+      "enable": true,
+      "frequency": 1,
+      "opacity": 1,
+      "shadow": {
+        "blur": 5,
+        "color": {
+          "value": "#00ff00"
+        },
+        "enable": false
+      },
+      "triangles": {
+        "enable": false,
+        "frequency": 1
+      },
+      "width": 1,
+      "warp": false
+    },
+    /*DIRI LAST MAG WORK ANG PARTICLES*/
+    "move": {
+      "angle": {
+        "offset": 0,
+        "value": 90
+      },
+      "attract": {
+        "distance": 200,
+        "enable": false,
+        "rotate": {
+          "x": 3000,
+          "y": 3000
+        }
+      },
+      "decay": 0,
+      "distance": {},
+      "direction": "none",
+      "drift": 0,
+      "enable": true,
+      "gravity": {
+        "acceleration": 9.81,
+        "enable": false,
+        "inverse": false,
+        "maxSpeed": 50
+      },
+      "path": {
+        "clamp": true,
+        "delay": {
+          "random": {
+            "enable": false,
+            "minimumValue": 0
+          },
+          "value": 0
+        },
+        "enable": false,
+        "options": {}
+      },
+      "outModes": {
+        "default": "out"
+      },
+      "random": false,
+      "size": false,
+      "speed": 2,
+      "spin": {
+        "acceleration": 0,
+        "enable": false
+      },
+      "straight": false,
+      "trail": {
+        "enable": false,
+        "length": 10,
+        "fillColor": {
+          "value": "#000000"
+        }
+      },
+      "vibrate": false,
+      "warp": false
+    },
+    "number": {
+      "density": {
+        "enable": false,
+        "area": 800,
+        "factor": 1000
+      },
+      "limit": 0,
+      "value": 100
+    },
+    "opacity": {
+      "random": {
+        "enable": false,
+        "minimumValue": 0.1
+      },
+      "value": 1,
+      "animation": {
+        "count": 0,
+        "enable": false,
+        "speed": 2,
+        "sync": false,
+        "destroy": "none",
+        "startValue": "random"
+      }
+    },
+    "orbit": {
+      "animation": {
+        "count": 0,
+        "enable": false,
+        "speed": 1,
+        "sync": false
+      },
+      "enable": false,
+      "opacity": 1,
+      "rotation": {
+        "random": {
+          "enable": false,
+          "minimumValue": 0
+        },
+        "value": 45
+      },
+      "width": 1
+    },
+    "reduceDuplicates": false,
+    "repulse": {
+      "random": {
+        "enable": false,
+        "minimumValue": 0
+      },
+      "value": 0,
+      "enabled": false,
+      "distance": 1,
+      "duration": 1,
+      "factor": 1,
+      "speed": 1
+    },
+    "roll": {
+      "darken": {
+        "enable": false,
+        "value": 0
+      },
+      "enable": false,
+      "enlighten": {
+        "enable": false,
+        "value": 0
+      },
+      "mode": "vertical",
+      "speed": 25
+    },
+    "rotate": {
+      "random": {
+        "enable": false,
+        "minimumValue": 0
+      },
+      "value": 0,
+      "animation": {
+        "enable": false,
+        "speed": 0,
+        "sync": false
+      },
+      "direction": "clockwise",
+      "path": false
+    },
+    "shadow": {
+      "blur": 0,
+      "color": {
+        "value": "#000000"
+      },
+      "enable": false,
+      "offset": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    "shape": {
+      "options": {},
+      "type": "circle"
+    },
+    "size": {
+      "random": {
+        "enable": false,
+        "minimumValue": 1
+      },
+      "value": 1,
+      "animation": {
+        "count": 0,
+        "enable": false,
+        "speed": 5,
+        "sync": false,
+        "destroy": "none",
+        "startValue": "random"
+      }
+    },
+    "stroke": {
+      "width": 0
+    },
+    "tilt": {
+      "random": {
+        "enable": false,
+        "minimumValue": 0
+      },
+      "value": 0,
+      "animation": {
+        "enable": false,
+        "speed": 0,
+        "sync": false
+      },
+      "direction": "clockwise",
+      "enable": false
+    },
+    "twinkle": {
+      "lines": {
+        "enable": false,
+        "frequency": 0.05,
+        "opacity": 1
+      },
+      "particles": {
+        "enable": false,
+        "frequency": 0.05,
+        "opacity": 1
+      }
+    },
+    "wobble": {
+      "distance": 5,
+      "enable": false,
+      "speed": 50
+    },
+    "zIndex": {
+      "random": {
+        "enable": false,
+        "minimumValue": 0
+      },
+      "value": 0,
+      "opacityRate": 1,
+      "sizeRate": 1,
+      "velocityRate": 1
+    }
+  },
+  "pauseOnBlur": true,
+  "pauseOnOutsideViewport": true,
+  "responsive": [],
+  "style": {},
+  "themes": [],
+  "zLayers": 100
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={ParticlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*
+                <FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
