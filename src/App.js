3e2bbb43d3b8293734f6e140d5f6f7cb59ea110d
unknown <jpearlz0123@gmail.com>
2022-02-02 23:24:32 +0800

    working smartbrain

--- src/App.js
+++ src/App.js
@@ -8,16 +8,32 @@ import { ParticlesOptions } from './Particles';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className="App">
-        <Particles className='particles'
-          params={ParticlesOptions}
-        />
+        <Particles className='particles' params={ParticlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} 
+        />
         {/*
                 <FaceRecognition />*/}
       </div>
